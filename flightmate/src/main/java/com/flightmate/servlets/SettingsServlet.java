235ce35a520caa92837582c9b75870543fb4bdf7
Gabriel Montplaisir <gabriel.montplaisir@protonmail.com>
2024-11-02 02:33:37 -0400

    Setup settings page and ability to edit personal info

---
+++ flightmate/src/main/java/com/flightmate/servlets/SettingsServlet.java
@@ -0,0 +1,141 @@
+package com.flightmate.servlets;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.flightmate.beans.User;
+import com.flightmate.dao.UserDao;
+import com.flightmate.libs.Validation;
+import com.flightmate.libs.services.SessionService;
+
+@WebServlet("/settings")
+public class SettingsServlet extends Validation {
+	private static final long serialVersionUID = 1L;
+	private final String SETTINGS_JSP = "settings.jsp";
+	private final String ACTION_INFO = "action_info";
+	private final String ACTION_PASS = "action_pass";
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		User user = SessionService.srv.getSessionUser(req);
+		
+		if (user == null) {
+			resp.sendRedirect("../login");
+			return;
+		}
+		
+		if (req.getAttribute(ACTION_INFO) == null) req.setAttribute(ACTION_INFO, "disabled");
+		if (req.getAttribute(ACTION_PASS) == null) req.setAttribute(ACTION_PASS, "disabled");
+		
+		req.getRequestDispatcher("./"+SETTINGS_JSP).forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		User user = SessionService.srv.getSessionUser(req);
+		
+		String message = null;
+		boolean updated = false;
+		
+		if (user == null) {
+			resp.sendRedirect("./login");
+			return;
+		}
+		
+		// Update User Information
+		String action_info = req.getParameter(ACTION_INFO) != null ? req.getParameter(ACTION_INFO) : "";
+		String action_pass = req.getParameter(ACTION_PASS) != null ? req.getParameter(ACTION_PASS) : "";
+		
+		if (action_info.equals("update")) {
+			req.setAttribute(ACTION_INFO, "");
+		} else if (action_info.equals("save")) {
+			req.setAttribute(ACTION_INFO, "disabled");
+			message = validateForm(req.getParameterMap());
+			
+			String firstName = req.getParameter("firstname");
+			String lastName = req.getParameter("lastname");
+			String email = req.getParameter("email");
+			
+			if (!user.getEmail().equals(email) && UserDao.getDao().userExists(email)) message = "User with that email already exists. Cannot update email.";
+			if (user.getFirstName().equals(firstName) 
+					&& user.getLastName().equals(lastName) 
+					&& user.getEmail().equals(email) 
+				) {
+				message = "No info updated.";
+			}
+			if (message != null) {
+				req.setAttribute("message", message);
+			} else {
+				user.setFirstName(firstName);
+				user.setLastName(lastName);
+				user.setEmail(email);
+				
+				updated = UserDao.getDao().updateUser(firstName, lastName, email, user.getUserId());
+				if (updated) {
+					req.setAttribute("success", "Profile updated.");
+				}
+			}
+			
+			
+		}
+		
+		// Update Password
+		if (action_pass.equals("update")) {
+			req.setAttribute(ACTION_PASS, "");
+		} else if (action_pass.equals("save")) {
+			req.setAttribute(ACTION_PASS, "disabled");
+			String currentPass = req.getParameter("password");
+			String newPass = req.getParameter("new_password");
+			String confirmNew = req.getParameter("confirm_new");
+			
+			message = validatePasswordForm(currentPass, newPass, confirmNew, user);
+			
+			if (message != null) {
+				req.setAttribute("message", message);
+			} else {
+				updated = UserDao.getDao().updatePassword(user.getUserId(), newPass);
+				if (updated) {
+					req.setAttribute("success", "Password updated.");
+				}
+			}
+		}
+				
+		doGet(req, resp);
+	}
+	
+	@Override
+	protected String validateForm(Map<String, String[]> params) {
+		Iterator<Entry<String, String[]>> iterator = params.entrySet().iterator();
+		
+		while (iterator.hasNext()) {
+			Map.Entry<String, String[]> entry = (Entry<String, String[]>)iterator.next();
+			String paramName = entry.getKey();
+			String paramValue = entry.getValue()[0];
+			
+			if (isEmpty(paramName)) {return paramName+" cannot be empty.";}
+			if (paramName.equals("email")) {
+				if (!paramValue.matches(emailRegEx)) return "Email is invalid.";
+				
+			}
+			
+			
+		}
+		return null;
+	}
+	
+	protected String validatePasswordForm(String currentPass, String newPass, String confirmNew, User user) {
+		if (isEmpty(currentPass)) return "Password cannot be empty.";
+		if (!isEmpty(currentPass) && (isEmpty(newPass) || isEmpty(confirmNew))) return "New password cannot be blank and needs to be confirmed.";
+		if (currentPass.equals(newPass)) return "New password cannot be identical to current password";
+		if (!confirmNew.equals(newPass)) return "New password and confirmation do not match";
+		if (!UserDao.getDao().passwordMatches(user.getEmail(), currentPass)) return "Password does not match account password.";
+		return null;	
+	}
+}
---
+++ flightmate/src/main/webapp/settings.jsp
@@ -0,0 +1,66 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<jsp:include page="./components/head.jsp" />
+	<link rel="stylesheet" href="${pageContext.request.contextPath}/css/dashboard.css">
+</head>
+<body class="background">
+	<jsp:include page='./components/header.jsp' />
+	<main>
+		<header class="main_header">
+			<h1 class="subtitle">Settings</h1>
+		</header>
+		<div class="flex gap-8 w-72">
+			<section class="container mt-2 flex-2">
+				<h2 class="subtitle">Your information</h2>
+				<form action="settings" method="POST">
+					<input type="hidden" name="action_info" value="${action_info.isBlank() ? 'save' : 'update'}">
+					<div class="flex my-2">
+						<label for="fname" class="form_label flex-1">First Name:</label><input type="text" id="fname" name="firstname" class="form_input w-full flex-2" value="${user.getFirstName()}" ${action_info} >
+						
+					</div>
+					<div class="flex my-2">
+						<label for="lname" class="form_label flex-1">Last Name: </label><input type="text" id="lname" name="lastname" class="form_input w-full flex-2" value="${user.getLastName()}" ${action_info}>
+					</div>
+					<div class="flex my-2">
+						<label for="email" class="form_label flex-1">Email address: </label><input type="email" id="email" name="email" class="form_input w-full flex-2" value="${user.getEmail()}" ${action_info}>
+					</div>
+					<c:if test="${action_info.isBlank()}">
+						<input type="submit" name="submit" value="Save" class="btn">
+						<input type="button" value="Cancel" onclick="window.location='${pageContext.request.contextPath}/settings'" class="btn ml-2 cancel">
+					</c:if>
+					<c:if test="${action_info.equals('disabled')}">
+						<input type="submit" name="submit" value="Update Info" class="btn">
+					</c:if>
+				</form>
+				<form action="settings" method="POST">
+					<input type="hidden" name="action_pass" value="${action_pass.isBlank() ? 'save' : 'update'}">
+					<div class="flex my-2">
+						<label for="password" class="form_label flex-1">Current Password: </label><input type="password" id="password" name="password" class="form_input w-full flex-2" value="placeholder" ${action_pass}>
+					</div>
+					<c:if test="${action_pass.isBlank()}">
+						<div class="flex my-2">
+							<label for="new_password" class="form_label flex-1">New Password: </label><input type="password" id="new_password" name="new_password" class="form_input w-full flex-2">
+						</div>
+						<div class="flex my-2">
+							<label for="confirm_new" class="form_label flex-1">Confirm New Password: </label><input type="password" id="confirm_new" name="confirm_new" class="form_input w-full flex-2">
+						</div>
+						<input type="submit" name="submit" value="Save" class="btn">
+						<input type="button" value="Cancel" onclick="window.location='${pageContext.request.contextPath}/settings'" class="btn ml-2 cancel">
+					</c:if>
+					<c:if test="${action_pass.equals('disabled')}">
+						<input type="submit" name="submit" value="Update Password" class="btn">
+					</c:if>
+				</form>
+				<p class="message_error mt-2">${message}</p>
+				<p class="message_success mt-2">${success}</p>
+			</section>
+
+			
+		</div>
+	</main>
+</body>
+</html>
