8a65a3efc103e44fa87539b2cc96a2dc76564242
Gabriel Montplaisir <52639192+GabrielMontplaisir@users.noreply.github.com>
2024-11-28 22:32:03 -0500

    Fix javax imports from PR#7 (#8)
    Emily's branch forgot to import the javax functions. This fixes that.

--- flightmate/src/main/java/com/flightmate/servlets/FlightStatusServlet.java
+++ flightmate/src/main/java/com/flightmate/servlets/FlightStatusServlet.java
@@ -3,6 +3,12 @@ package com.flightmate.servlets;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.flightmate.dao.FlightDao;
 
 
